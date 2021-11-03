1fbad63da88c6d706c7f8e702126ff63ebe8dea2
isaquepim <izaq.pim@gmail.com>
2021-11-03 18:04:21 -0300

    update before class

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+
+app = Flask(__name__)
+
+texts = []
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def get_text():
+    new_text = request.form.get('get_text')
+    texts.append(new_text)
+    print(texts)
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug=True, port=40)
